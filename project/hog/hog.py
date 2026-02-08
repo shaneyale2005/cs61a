c3090e1ec49f6649f22b07ab6315cda04a908b09
shaneyale <zeta186012@gmail.com>
2026-02-08 19:03:09 +0800

    Implement boar_brawl check in boar_strategy

--- project/hog/hog.py
+++ project/hog/hog.py
@@ -330,6 +330,8 @@ def boar_strategy(score, opponent_score, threshold=11, num_rolls=6):
     points, and returns NUM_ROLLS otherwise. Ignore the Sus Fuss rule.
     """
     # BEGIN PROBLEM 10
+    if boar_brawl(score, opponent_score) >= threshold:
+        return 0
     return num_rolls  # Remove this line once implemented.
     # END PROBLEM 10
 
